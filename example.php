0404b851e7d97853c2857ef95a8e5dc95f02f100
Igor Belikov <work.belka@gmail.com>
2015-06-06 05:29:06 +0300

    Update example.php

--- example.php
+++ example.php
@@ -2,22 +2,23 @@
 
 require "vendor/autoload.php";
 
-$keys = [
-	'AIzaSyDdUg1Ppu37T2mXhrHrUksLg1ZGSOB8eLA', // sapa-2
-	'AIzaSyDdUg1Ppu37T2mXhrHrUksLg1ZGSOB8eLA', // sapa-2
-	'AIzaSyDdUg1Ppu37T2mXhrHrUksLg1ZGSOB8eLA', // sapa-2
-	'AIzaSyDDLHySG-_6dTx1IbLo6BBUX4qi4sZ6BYI', // sapa-1
-];
+// https://console.developers.google.com/project
+$keys = array(
+    'YOUR_DEVELOPER_KEY_1', // app-1
+    'YOUR_DEVELOPER_KEY_2', // app-2
+    'YOUR_DEVELOPER_KEY_3', // app-3
+    // ...
+);
 
 $client = new Google_Client_Multi();
 $client->setKeys($keys)->prepareMulti();
 
 $service = new Google_Service_Customsearch($client);
 try {
-	$cse = $service->cse->listCse("weather", array('cx' => '005037261954542973463:0bv5enakkcc'));
-	var_dump($cse->getItems());
+    $cse = $service->cse->listCse("weather", array('cx' => 'YOUR_CUSTOM_SEARCH_ENGINE_ID'));
+    var_dump($cse->getItems());
 } catch(Google_Service_Exception $e) {
-	echo $e->getMessage();
+    echo $e->getMessage();
 } catch(Google_Client_Multi_Exception $e) {
-	echo $e->getMessage();
+    echo $e->getMessage();
 }
